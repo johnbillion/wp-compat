f6f1e5ba7ba6e22b7e5c615f2d6f52b4d4ed14fd
John Blackbourn <john@johnblackbourn.com>
2024-09-04 12:16:55 +0100

    More coding standards.

--- src/Rules/SinceVersionRule.php
+++ src/Rules/SinceVersionRule.php
@@ -18,7 +18,7 @@ class SinceVersionRule implements Rule {
 
 	public function __construct() {
 		$symbolsFilePath = dirname( __DIR__, 2 ) . '/symbols.json';
-		$this->symbols = json_decode(file_get_contents($symbolsFilePath), true)['symbols'];
+		$this->symbols = json_decode( file_get_contents( $symbolsFilePath ), true )['symbols'];
 		$this->minVersion = '6.0';
 	}
 
@@ -30,20 +30,20 @@ class SinceVersionRule implements Rule {
 	 * @param CallLike $node
 	 * @return list<RuleError>
 	 */
-	public function processNode(Node $node, Scope $scope): array {
+	public function processNode( Node $node, Scope $scope ): array {
 		if ( ( ! $node instanceof FuncCall ) && ( ! $node instanceof MethodCall ) && ( ! $node instanceof StaticCall ) ) {
 			return [];
 		}
 
-		$name = self::getNodeName($node);
+		$name = self::getNodeName( $node );
 
-		if ( ! is_string( $name ) || ! isset($this->symbols[$name] ) ) {
+		if ( ! is_string( $name ) || ! isset( $this->symbols[ $name ] ) ) {
 			return [];
 		}
 
-		$since = $this->symbols[$name]['since'];
+		$since = $this->symbols[ $name ]['since'];
 
-		if (version_compare($since, $this->minVersion, '<=')) {
+		if ( version_compare( $since, $this->minVersion, '<=' ) ) {
 			return [];
 		}
 
@@ -54,20 +54,20 @@ class SinceVersionRule implements Rule {
 		);
 
 		return [
-			RuleErrorBuilder::message($message)->identifier('WPCompat.nope')->build(),
+			RuleErrorBuilder::message( $message )->identifier( 'WPCompat.nope' )->build(),
 		];
 	}
 
-	private static function getNodeName(Node $node): ?string {
-		if ($node instanceof FuncCall) {
+	private static function getNodeName( Node $node ): ?string {
+		if ( $node instanceof FuncCall ) {
 			return (string) $node->name;
 		}
 
-		if ($node instanceof MethodCall) {
+		if ( $node instanceof MethodCall ) {
 			return (string) $node->name;
 		}
 
-		if ($node instanceof StaticCall) {
+		if ( $node instanceof StaticCall ) {
 			return (string) $node->name;
 		}
 
